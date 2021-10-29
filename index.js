d1b03725e69afb538d738161796a71b5dcdc7e3f
Dayeane <dayeane85@gmail.com>
2021-10-29 11:43:27 -0700

    commit tree a4b8cd3e9702babb5d56bceed05be3e389b0371e

--- index.js
+++ index.js
@@ -1,1 +1,76 @@
 // Your code here
+function createEmployeeRecord(employee){
+    const employObj = {
+        firstName: employee[0],
+        familyName: employee[1],
+        title: employee[2],
+        payPerHour: employee[3],
+        timeInEvents:[],
+        timeOutEvents: [], 
+    
+    }
+    return employObj
+
+
+}
+    
+function createEmployeeRecords (employees) {
+    let arrayContainer  = []
+
+
+    for (let i = 0; i < employees.length; i++){
+      arrayContainer.push(createEmployeeRecord(employees[i]))
+        
+    }
+
+    return arrayContainer 
+}
+
+function createTimeInEvent(employeeRec, dateTime) {
+    const timeEvent = {
+        type: "TimeIn",
+        hour: parseInt(dateTime.split(" ")[1]),
+        date: dateTime.split(" ")[0],
+    }
+
+    employeeRec.timeInEvents.push(timeEvent)
+    return employeeRec
+}
+
+function createTimeOutEvent(employeeRec, dateTime) {
+    const timeEventOut = {
+        type: "TimeOut",
+        hour: parseInt(dateTime.split(" ")[1]),
+        date: dateTime.split(" ")[0],
+    }
+
+    employeeRec.timeOutEvents.push(timeEventOut)
+    return employeeRec
+}
+
+function hoursWorkedOnDate(employeeRec, dateTime) {
+            
+        let varIn = employeeRec.timeInEvents.find((timeEvent) => timeEvent.date === dateTime)
+
+        let varOut = employeeRec.timeOutEvents.find((timeEventOut) => timeEventOut.date === dateTime)
+
+           return (varOut.hour - varIn.hour)/100
+
+}
+
+function wagesEarnedOnDate(employeeRec, dateTime) {
+            
+    return hoursWorkedOnDate(employeeRec,dateTime)*employeeRec.payPerHour
+
+}
+
+function allWagesFor(employeeRec) {
+
+    let addAllWages = 0 
+    employeeRec.timeInEvents.forEach((timeEvent) =>{
+        addAllWages += wagesEarnedOnDate(employeeRec,timeEvent.date)    
+    })
+    
+   return addAllWages
+
+}
